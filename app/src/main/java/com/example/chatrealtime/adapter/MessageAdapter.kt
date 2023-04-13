0937b2c865b570c931cc8ea22ea087e4c200c421
asmaaalastal <105522153+asmaaalastal@users.noreply.github.com>
2023-04-13 16:27:25 +0400

    Chat TIME Success

--- app/src/main/java/com/example/chatrealtime/adapter/MessageAdapter.kt
+++ app/src/main/java/com/example/chatrealtime/adapter/MessageAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.chatrealtime.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +11,9 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatrealtime.R
+import java.text.DateFormat
+import java.util.*
+
 
 class MessagesAdapter(
     private val context: Context,
@@ -18,7 +23,7 @@ class MessagesAdapter(
 
     class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val messageText: TextView = itemView.findViewById(R.id.message_text)
-       // val messageTime : TextView = itemView.findViewById(R.id.message_time)
+        val messageTime : TextView = itemView.findViewById(R.id.message_time)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
@@ -29,14 +34,15 @@ class MessagesAdapter(
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
          holder.messageText.text = message.text
-//        val time = messages[position]
-//        holder.messageTime.text = time.text
+        val dateFormat = DateFormat.getDateTimeInstance()
+        holder.messageTime.text = dateFormat.format(Date(message.timestamp)).toString()
 
         val layoutParams = holder.messageText.layoutParams as
                 LinearLayout.LayoutParams
         layoutParams.gravity = if (message.senderId == currentUserUid)
             Gravity.END else Gravity.START
         holder.messageText.layoutParams = layoutParams
+        holder.messageTime.layoutParams = layoutParams
 
 
     }
@@ -44,4 +50,5 @@ class MessagesAdapter(
     override fun getItemCount(): Int {
         return messages.size
     }
+
 }
--- app/src/main/java/com/example/chatrealtime/chat.kt
+++ app/src/main/java/com/example/chatrealtime/chat.kt
@@ -11,53 +11,50 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatrealtime.adapter.Message
 import com.example.chatrealtime.adapter.MessagesAdapter
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.message_item.*
 
 class chat : AppCompatActivity() {
+    private lateinit var messagesRef: DatabaseReference
     private lateinit var recyclerMessage: RecyclerView
     private lateinit var message: EditText
     private lateinit var send: Button
     private lateinit var senderUid: String
     private lateinit var receiverUid:String
+    val messageList = mutableListOf<Message>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
-        recyclerMessage = recyclerView_message
-        message = message_input
-        send = btn_send
+        recyclerMessage = findViewById(R.id.recyclerView_message)
+        message = findViewById(R.id.message_input)
+        send = findViewById(R.id.btn_send)
         senderUid = "Yeh0MqTJyXfyKWdr3BAohfn4GDz1"
         receiverUid = "jjcMjL53XbXA9wiUqmoUCAoWkuI3"
         send.setOnClickListener {
             val messageText = message.text.toString().trim()
+            Log.d("messageText", messageText)
             if (messageText.isNotEmpty()){
                 messageSend(messageText)
                 message.setText("")
             }
         }
-        val messageList = mutableListOf<Message>()
+        messagesRef = FirebaseDatabase.getInstance().getReference("chat")
         val messageAdapter = MessagesAdapter(this,messageList,senderUid)
         recyclerMessage.layoutManager = LinearLayoutManager(this)
         recyclerMessage.adapter = messageAdapter
-        FirebaseDatabase.getInstance().getReference("chat").addChildEventListener(object :
+        messagesRef.addChildEventListener(object :
             ChildEventListener {
             override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                 val message = snapshot.getValue(Message::class.java)
-//                val time = snapshot.getValue(Message::class.java)
                 if (message!= null){
                     messageList.add(message)
                     Log.d("messageDisplay", "Success")
-//                    if (time != null) {
-//                        messageList.add(time)
-//                    }
+                    messageAdapter.notifyItemInserted(messageList.size -1)
+                    recyclerMessage.scrollToPosition(messageList.size -1)
                 }
-                messageAdapter.notifyItemInserted(messageList.size -1)
-                recyclerMessage.scrollToPosition(messageList.size -1)
+
             }
             override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
 
@@ -77,7 +74,9 @@ class chat : AppCompatActivity() {
     private fun messageSend(messageText:String){
         val timestamp = System.currentTimeMillis()
         val message = Message(messageText,senderUid,receiverUid,timestamp)
+        Log.e("Asmaa","1")
         FirebaseDatabase.getInstance().getReference("chat").push().setValue(message)
+        Log.e("Asmaa","1")
         Log.d("messageSend", "Success")
     }
 }
